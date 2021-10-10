5609739372402dae81ecd3c94223deb8d8eb098a
Antoniya <anemtserova@gmail.com>
2021-10-10 08:14:08 +0000

    commit

--- src/api/routes.py
+++ src/api/routes.py
@@ -4,7 +4,8 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 
 import os
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
+from flask_migrate import Migrate
+from api.models import db, User, Contact, Note
 from api.utils import generate_sitemap, APIException
 
 from flask_jwt_extended import create_access_token
