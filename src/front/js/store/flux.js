ae6b98b3d38772a40d9eef0355cd191e08cf1a10
Antoniya <anemtserova@gmail.com>
2021-10-12 14:59:39 +0000

    start integrating backend with front... not sure it's right tho

--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -4,24 +4,38 @@ const getState = ({ getStore, setStore, getActions }) => {
 			contacts: [],
 			token: null,
 			message: "",
-			noteArray: JSON.parse(localStorage.getItem("notes")) || []
+			// noteArray: JSON.parse(localStorage.getItem("notes")) || []
+			allUsers: []
 		},
 		actions: {
-			deleteNote: noteIndex => {
-				const prevNotes = getStore().noteArray;
-				const notesNotDeleted = prevNotes.filter((el, i) => {
-					return i != noteIndex;
-				});
-				setStore({ noteArray: notesNotDeleted });
-				localStorage.setItem("notes", JSON.stringify(notesNotDeleted));
-			},
-			deleteAllUserNotes: id => {
-				const prevNotes = getStore().noteArray;
-				const notesNotDeleted = prevNotes.filter(el => {
-					return el.userId != id;
-				});
-				setStore({ noteArray: notesNotDeleted });
-				localStorage.setItem("notes", JSON.stringify(notesNotDeleted));
+			// deleteNote: noteIndex => {
+			// 	const prevNotes = getStore().noteArray;
+			// 	const notesNotDeleted = prevNotes.filter((el, i) => {
+			// 		return i != noteIndex;
+			// 	});
+			// 	setStore({ noteArray: notesNotDeleted });
+			// 	localStorage.setItem("notes", JSON.stringify(notesNotDeleted));
+			// },
+			// deleteAllUserNotes: id => {
+			// 	const prevNotes = getStore().noteArray;
+			// 	const notesNotDeleted = prevNotes.filter(el => {
+			// 		return el.userId != id;
+			// 	});
+			// 	setStore({ noteArray: notesNotDeleted });
+			// 	localStorage.setItem("notes", JSON.stringify(notesNotDeleted));
+			// },
+			getAllUsers: () => {
+				fetch(process.env.BACKEND_URL + "/api/users", opts)
+				.then(response => {
+					if (!response.ok) {
+						throw Error(response.statusText);
+					}
+					return response.json();
+				})
+				.then(allUsers => {
+					setStore({ allUsers: allUsers });
+				})
+				.catch(err => console.log("There was a following error: " + err));
 			},
 			saveTokenFromSessionStorage: () => {
 				const token = sessionStorage.getItem("token");
@@ -62,40 +76,40 @@ const getState = ({ getStore, setStore, getActions }) => {
 					console.error("There has been an error while loging in.");
 				}
 			},
-			editFetch: person => {
-				fetch("https://assets.breatheco.de/apis/fake/contact/" + person.id, {
-					method: "PUT",
-					headers: {
-						"Content-Type": "application/json"
-					},
-					body: JSON.stringify({
-						full_name: person.full_name,
-						email: person.email,
-						agenda_slug: "agenda_2025",
-						address: person.address,
-						phone: person.phone,
-						note: person.note
-					})
-				})
-					.then(response => {
-						if (!response.ok) {
-							throw Error(response.statusText);
-						}
-						return response.json();
-					})
-					.then(data => {
-						const prevNotes = getStore().noteArray;
-						if (person.note != "" && person.note != undefined) {
-							setStore({ noteArray: [...prevNotes, { userId: data.id, note: person.note }] });
-						}
-						const editedNotes = getStore().noteArray;
-						console.log("Edited Notes Array", editedNotes);
-						localStorage.setItem("notes", JSON.stringify(editedNotes));
-						getActions().getFetch();
-						// confirm return of data here
-					})
-					.catch(err => console.log("There was a following error: " + err));
-			},
+			// editFetch: person => {
+			// 	fetch("https://assets.breatheco.de/apis/fake/contact/" + person.id, {
+			// 		method: "PUT",
+			// 		headers: {
+			// 			"Content-Type": "application/json"
+			// 		},
+			// 		body: JSON.stringify({
+			// 			full_name: person.full_name,
+			// 			email: person.email,
+			// 			agenda_slug: "agenda_2025",
+			// 			address: person.address,
+			// 			phone: person.phone,
+			// 			note: person.note
+			// 		})
+			// 	})
+			// 		.then(response => {
+			// 			if (!response.ok) {
+			// 				throw Error(response.statusText);
+			// 			}
+			// 			return response.json();
+			// 		})
+			// 		.then(data => {
+			// 			const prevNotes = getStore().noteArray;
+			// 			if (person.note != "" && person.note != undefined) {
+			// 				setStore({ noteArray: [...prevNotes, { userId: data.id, note: person.note }] });
+			// 			}
+			// 			const editedNotes = getStore().noteArray;
+			// 			console.log("Edited Notes Array", editedNotes);
+			// 			localStorage.setItem("notes", JSON.stringify(editedNotes));
+			// 			getActions().getFetch();
+			// 			// confirm return of data here
+			// 		})
+			// 		.catch(err => console.log("There was a following error: " + err));
+			// },
 
 			greetUser: () => {
 				const store = getStore();
@@ -111,72 +125,72 @@ const getState = ({ getStore, setStore, getActions }) => {
 					.catch(err => console.log("There has been an error loading message from backend", err));
 			},
 
-			deleteFetch: id => {
-				fetch("https://assets.breatheco.de/apis/fake/contact/" + id, {
-					method: "DELETE"
-				})
-					.then(response => {
-						if (!response.ok) {
-							throw Error(response.statusText);
-						}
-						return response.json();
-					})
-					.then(data => {
-						console.log("This is the deleteFetch data: ", data);
-						getActions().getFetch();
-						// confirm return of data here
-					})
-					.catch(err => console.log("There was a following error: " + err));
-			},
+			// deleteFetch: id => {
+			// 	fetch("https://assets.breatheco.de/apis/fake/contact/" + id, {
+			// 		method: "DELETE"
+			// 	})
+			// 		.then(response => {
+			// 			if (!response.ok) {
+			// 				throw Error(response.statusText);
+			// 			}
+			// 			return response.json();
+			// 		})
+			// 		.then(data => {
+			// 			console.log("This is the deleteFetch data: ", data);
+			// 			getActions().getFetch();
+			// 			// confirm return of data here
+			// 		})
+			// 		.catch(err => console.log("There was a following error: " + err));
+			// },
 
-			getFetch: () => {
-				fetch("https://assets.breatheco.de/apis/fake/contact/agenda/agenda_2025")
-					.then(response => {
-						if (!response.ok) {
-							throw Error(response.statusText);
-						}
-						return response.json();
-					})
-					.then(data => {
-						setStore({ contacts: data });
-					})
-					.catch(err => console.log("There was a following error: " + err));
-			},
+			// getFetch: () => {
+			// 	fetch("https://assets.breatheco.de/apis/fake/contact/agenda/agenda_2025")
+			// 		.then(response => {
+			// 			if (!response.ok) {
+			// 				throw Error(response.statusText);
+			// 			}
+			// 			return response.json();
+			// 		})
+			// 		.then(data => {
+			// 			setStore({ contacts: data });
+			// 		})
+			// 		.catch(err => console.log("There was a following error: " + err));
+			// },
 
-			postFetch: contact => {
-				fetch("https://assets.breatheco.de/apis/fake/contact/", {
-					method: "POST",
-					headers: {
-						"Content-Type": "application/json"
-					},
-					body: JSON.stringify({
-						full_name: contact.name,
-						email: contact.email,
-						agenda_slug: "agenda_2025",
-						address: contact.address,
-						phone: contact.phone,
-						note: contact.note
-					})
-				})
-					.then(response => {
-						if (!response.ok) {
-							throw Error(response.statusText);
-						}
-						return response.json();
-					})
-					.then(data => {
-						const prevNotes = getStore().noteArray;
-						if (contact.note != "" && contact.note != undefined) {
-							setStore({ noteArray: [...prevNotes, { userId: data.id, note: contact.note }] });
-						}
-						const currNotes = getStore().noteArray;
-						localStorage.setItem("notes", JSON.stringify(currNotes));
-						getActions().getFetch();
-						console.log("This is the POST fetch data ", data);
-						// confirm return of data here
-					})
-					.catch(err => console.log("There was a following error: " + err));
-			}
+			// postFetch: contact => {
+			// 	fetch("https://assets.breatheco.de/apis/fake/contact/", {
+			// 		method: "POST",
+			// 		headers: {
+			// 			"Content-Type": "application/json"
+			// 		},
+			// 		body: JSON.stringify({
+			// 			full_name: contact.name,
+			// 			email: contact.email,
+			// 			agenda_slug: "agenda_2025",
+			// 			address: contact.address,
+			// 			phone: contact.phone,
+			// 			note: contact.note
+			// 		})
+			// 	})
+			// 		.then(response => {
+			// 			if (!response.ok) {
+			// 				throw Error(response.statusText);
+			// 			}
+			// 			return response.json();
+			// 		})
+			// 		.then(data => {
+			// 			const prevNotes = getStore().noteArray;
+			// 			if (contact.note != "" && contact.note != undefined) {
+			// 				setStore({ noteArray: [...prevNotes, { userId: data.id, note: contact.note }] });
+			// 			}
+			// 			const currNotes = getStore().noteArray;
+			// 			localStorage.setItem("notes", JSON.stringify(currNotes));
+			// 			getActions().getFetch();
+			// 			console.log("This is the POST fetch data ", data);
+			// 			// confirm return of data here
+			// 		})
+			// 		.catch(err => console.log("There was a following error: " + err));
+			// }
 		}
 	};
 };
