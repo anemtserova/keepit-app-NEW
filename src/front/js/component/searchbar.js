7a74175af239f43aea747b945cc5c6893debdd21
Antoniya <anemtserova@gmail.com>
2021-10-16 23:14:11 +0000

    tryin some stuff but not workin...

---
+++ src/front/js/component/searchbar.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect, useContext } from "react";
+import { withRouter } from "react-router-dom";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+import { GlobalState } from "../store/appContext";
+
+export const SearchBar = () => {
+    const { store, actions } = useContext(GlobalState);
+    
+
+
+}
