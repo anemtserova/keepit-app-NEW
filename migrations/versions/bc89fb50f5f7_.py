4f233469827893dcd8443192c10dc22300925355
Antoniya <anemtserova@gmail.com>
2021-10-20 10:57:23 +0000

    ....

--- migrations/versions/bc89fb50f5f7_.py
+++ migrations/versions/bc89fb50f5f7_.py
@@ -12,7 +12,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = 'bc89fb50f5f7'
-down_revision = 'e432e34a5473'
+down_revision = None
 branch_labels = None
 depends_on = None
 
